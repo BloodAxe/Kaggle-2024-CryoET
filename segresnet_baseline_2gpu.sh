65728df8a40c0a7af80a9ceadda5ad46ab1f5faf
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2024-12-30 00:07:31 +0200

    OD

--- segresnet_baseline_2gpu.sh
+++ segresnet_baseline_2gpu.sh
@@ -1,7 +1,7 @@
 torchrun --standalone --nproc-per-node=2 train.py \
 --model_name=segresnet --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
 --per_device_train_batch_size=6 --window_size=128 --num_crops_per_study=128 \
---learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99  --ema\
+--learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99 --ema \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --early_stopping=10 --warmup_steps=64 --average_tokens_across_devices=True \
 --use_instance_crops=True --use_random_crops=True \
@@ -10,7 +10,7 @@ torchrun --standalone --nproc-per-node=2 train.py \
 torchrun --standalone --nproc-per-node=2 train.py \
 --model_name=segresnet --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
 --per_device_train_batch_size=6 --window_size=128 --num_crops_per_study=128 \
---learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99  --ema\
+--learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99 --ema \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --early_stopping=10 --warmup_steps=64 --average_tokens_across_devices=True \
 --use_instance_crops=True --use_random_crops=True \
@@ -19,7 +19,7 @@ torchrun --standalone --nproc-per-node=2 train.py \
 torchrun --standalone --nproc-per-node=2 train.py \
 --model_name=segresnet --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
 --per_device_train_batch_size=6 --window_size=128 --num_crops_per_study=128 \
---learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99  --ema\
+--learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99 --ema \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --early_stopping=10 --warmup_steps=64 --average_tokens_across_devices=True \
 --use_instance_crops=True --use_random_crops=True \
@@ -28,7 +28,7 @@ torchrun --standalone --nproc-per-node=2 train.py \
 torchrun --standalone --nproc-per-node=2 train.py \
 --model_name=segresnet --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
 --per_device_train_batch_size=6 --window_size=128 --num_crops_per_study=128 \
---learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99  --ema\
+--learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99 --ema \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --early_stopping=10 --warmup_steps=64 --average_tokens_across_devices=True \
 --use_instance_crops=True --use_random_crops=True \
@@ -37,7 +37,7 @@ torchrun --standalone --nproc-per-node=2 train.py \
 torchrun --standalone --nproc-per-node=2 train.py \
 --model_name=segresnet --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
 --per_device_train_batch_size=6 --window_size=128 --num_crops_per_study=128 \
---learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99  --ema\
+--learning_rate=1e-5 --adam_beta1=0.95 --adam_beta2=0.99 --ema \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --early_stopping=10 --warmup_steps=64 --average_tokens_across_devices=True \
 --use_instance_crops=True --use_random_crops=True \
