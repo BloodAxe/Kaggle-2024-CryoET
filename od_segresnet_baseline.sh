487358e50d4b6690d69e1c333b18855b36afea89
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-01 16:06:03 +0200

    OD

---
+++ od_segresnet_baseline.sh
@@ -0,0 +1,8 @@
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=segresnet \
+--pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
+--per_device_train_batch_size=4 --learning_rate=1e-5 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--num_train_epochs=50 --warmup_steps=32 --average_tokens_across_devices=True  --use_sliding_crops=True \
+--fold=0 --ema --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp
