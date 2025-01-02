ff1fbeeabb995b3ea0b74fb42a7890caeef67a0e
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-02 13:04:04 +0200

    Random crop dataset

--- od_segresnet_baseline.sh
+++ od_segresnet_baseline.sh
@@ -4,15 +4,41 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --per_device_train_batch_size=2 --learning_rate=1e-5 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=50 --warmup_steps=256 --average_tokens_across_devices=False  --use_instance_crops=True \
+--num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
 --fold=0 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3
 
-
-python train_od.py \
+torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnet \
 --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
---per_device_train_batch_size=3 --learning_rate=1e-5 \
+--per_device_train_batch_size=2 --learning_rate=1e-5 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=50 --warmup_steps=256  --use_sliding_crops=True \
---fold=0 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp
+--num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
+--fold=1 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=segresnet \
+--pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
+--per_device_train_batch_size=2 --learning_rate=1e-5 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
+--fold=2 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=segresnet \
+--pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
+--per_device_train_batch_size=2 --learning_rate=1e-5 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
+--fold=3 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=segresnet \
+--pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
+--per_device_train_batch_size=2 --learning_rate=1e-5 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
+--fold=4 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3
