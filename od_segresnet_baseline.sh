95cf0f70a8b9db1bae0a8f49467710887585dfd6
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-02 21:33:15 +0200

    Comment trained

--- od_segresnet_baseline.sh
+++ od_segresnet_baseline.sh
@@ -14,7 +14,7 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=1 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3  --ddp_find_unused_parameters=True
+--fold=1 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3  --ddp_find_unused_parameters=True --early_stopping=10
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnet \
@@ -23,7 +23,7 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=2 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True
+--fold=2 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=10
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnet \
@@ -32,7 +32,7 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=3 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True
+--fold=3 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=10
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnet \
@@ -41,4 +41,4 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --num_train_epochs=50 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=4 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True
+--fold=4 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=10
