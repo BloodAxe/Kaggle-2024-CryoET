777d2c5ca6b0f146b8e3c1b6e0849d712118e4a1
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-05 14:40:00 +0200

    Model

--- od_segresnet_v2.sh
+++ od_segresnet_v2.sh
@@ -4,8 +4,8 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --per_device_train_batch_size=6 --learning_rate=1e-4 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=0 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_centernet_nms=True
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=0 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnetv2 \
@@ -13,8 +13,8 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --per_device_train_batch_size=6 --learning_rate=1e-4 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=0 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_centernet_nms=True --use_l1_loss=True
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=1 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnetv2 \
@@ -22,8 +22,8 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --per_device_train_batch_size=6 --learning_rate=1e-4 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=0 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_centernet_nms=False
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=2 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnetv2 \
@@ -31,8 +31,8 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --per_device_train_batch_size=6 --learning_rate=1e-4 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=0 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_centernet_nms=True
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=3 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
 
 torchrun --standalone --nproc-per-node=4 train_od.py \
 --model_name=segresnetv2 \
@@ -40,6 +40,5 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --per_device_train_batch_size=6 --learning_rate=1e-4 \
 --adam_beta1=0.95 --adam_beta2=0.99 \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=False  --use_instance_crops=True --use_random_crops=True \
---fold=0 --train_modes=denoised,ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_centernet_nms=False
-
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=4 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
