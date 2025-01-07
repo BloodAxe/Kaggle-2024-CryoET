cc122b2c2296509b72b309bbbc7a94ce5a9b98c1
BloodAxe <ekhvedchenya@gmail.com>
2025-01-07 19:45:20 +0200

    UnetR

--- od_dynunet.sh
+++ od_dynunet.sh
@@ -5,4 +5,41 @@ torchrun --standalone --nproc-per-node=4 train_od.py \
 --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
 --y_rotation_limit=10 --x_rotation_limit=10 \
 --num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
---fold=0 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25
+--fold=0 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=dynunet --window_size=128 --num_crops_per_study=256 \
+--per_device_train_batch_size=8 --learning_rate=3e-4 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--y_rotation_limit=10 --x_rotation_limit=10 \
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=1 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=dynunet --window_size=128 --num_crops_per_study=256 \
+--per_device_train_batch_size=8 --learning_rate=3e-4 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--y_rotation_limit=10 --x_rotation_limit=10 \
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=2 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=dynunet --window_size=128 --num_crops_per_study=256 \
+--per_device_train_batch_size=8 --learning_rate=3e-4 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--y_rotation_limit=10 --x_rotation_limit=10 \
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=3 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
+
+torchrun --standalone --nproc-per-node=4 train_od.py \
+--model_name=dynunet --window_size=128 --num_crops_per_study=256 \
+--per_device_train_batch_size=8 --learning_rate=3e-4 \
+--adam_beta1=0.95 --adam_beta2=0.99 \
+--dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+--y_rotation_limit=10 --x_rotation_limit=10 \
+--num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+--fold=4 --train_modes=denoised --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25
+
