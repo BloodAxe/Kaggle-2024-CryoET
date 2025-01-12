921a7bd6af9b17180b7b6b31fabbc86a2aa57f72
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-12 14:56:54 +0200

    Remove old exp

--- od_unet3d_fat_2gpu.sh
+++
@@ -1,7 +0,0 @@
-torchrun --standalone --nproc-per-node=2 train_od.py \
---model_name=unet3d-fat \
---per_device_train_batch_size=16 --learning_rate=1e-4 \
---adam_beta1=0.95 --adam_beta2=0.99 \
---dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---num_train_epochs=100 --warmup_steps=64 --average_tokens_across_devices=True   --use_random_crops=True --use_instance_crops=True --ddp_find_unused_parameters=True \
---fold=0 --train_modes=ctfdeconvolved,isonetcorrected,wbp --max_grad_norm=3  --early_stopping=25
