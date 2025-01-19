4e6099ac52d7874aa7df2e198aa5cb7bebb6d8a2
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-19 17:07:21 +0200

    Tune epochs

--- od_dynunet_s2_4gpu.sh
+++ od_dynunet_s2_4gpu.sh
@@ -6,7 +6,7 @@ do
   --adam_beta1=0.95 --adam_beta2=0.99 \
   --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
   --y_rotation_limit=10 --x_rotation_limit=10 \
-  --num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+  --num_train_epochs=75 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
   --fold=$FOLD  --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25 --use_stride4=False \
   --copy_paste_prob=0.2 --random_erase_prob=0.2 --mixup_prob=0.2 --use_6_classes=True
 done
