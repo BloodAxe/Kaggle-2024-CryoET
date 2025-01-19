126824f337db5cff7a611b4a27dac2bb81b5456d
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-19 16:36:24 +0200

    Update scripts

--- od_segresnet_s2_4gpu.sh
+++ od_segresnet_s2_4gpu.sh
@@ -11,6 +11,7 @@ do
     --y_rotation_limit=10 --x_rotation_limit=10 \
     --num_train_epochs=150 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
     --fold=$FOLD  --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_stride4=False \
+    --copy_paste_prob=0.2 --random_erase_prob=0.2 --mixup_prob=0.2 \
     --use_6_classes=True
 
 done
