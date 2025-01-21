4da8c1d4748a9150ef225d7c8905bc6b4fa8dd19
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-21 23:16:01 +0200

    LiteHRNet

--- od_hrnet_s2_3gpu.sh
+++ od_hrnet_s2_3gpu.sh
@@ -1,8 +1,14 @@
-torchrun --standalone --nproc-per-node=3 train_od.py \
---model_name=convnext --use_offset_head=False \
---per_device_train_batch_size=8 --learning_rate=5e-5 \
---dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
---y_rotation_limit=15 --x_rotation_limit=15  \
---num_train_epochs=50 --early_stopping=50 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
---fold=0  --max_grad_norm=3 --ddp_find_unused_parameters=True --ema
+for FOLD in 0 1 2 3 4
+do
+  torchrun --standalone --nproc-per-node=4 train_od.py \
+  --model_name=hrnet  \
+  --per_device_train_batch_size=8 --learning_rate=5e-5 \
+  --adam_beta1=0.95 --adam_beta2=0.99 \
+  --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
+  --y_rotation_limit=10 --x_rotation_limit=10 \
+  --num_train_epochs=75 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
+  --fold=$FOLD  --max_grad_norm=3 --ddp_find_unused_parameters=True --early_stopping=25 --use_stride4=False --use_6_classes=True \
+  --copy_paste_prob=0.2 --random_erase_prob=0.2 --mixup_prob=0.2 --use_cross_entropy_loss=True --ema
+
+done
 
