9ed416c297969bb108a492466e32d326f5319aa1
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-22 09:40:28 +0200

    Save exception if it happens

--- od_dynunet_s2_4gpu.sh
+++ od_dynunet_s2_4gpu.sh
@@ -2,11 +2,29 @@ for FOLD in 0 1 2 3 4
 do
   torchrun --standalone --nproc-per-node=4 train_od.py \
   --model_name=dynunet  \
-  --per_device_train_batch_size=6 --learning_rate=3e-4 \
-  --adam_beta1=0.95 --adam_beta2=0.99 \
-  --dataloader_num_workers=4 --dataloader_persistent_workers=True --dataloader_pin_memory=True \
-  --y_rotation_limit=10 --x_rotation_limit=10 \
-  --num_train_epochs=75 --warmup_steps=64 --average_tokens_across_devices=True  --use_instance_crops=True --use_random_crops=True \
-  --fold=$FOLD  --max_grad_norm=3 --ddp_find_unused_parameters=True --ema --early_stopping=25 --use_stride4=False \
-  --copy_paste_prob=0.2 --random_erase_prob=0.2 --mixup_prob=0.2 --use_6_classes=True --use_cross_entropy_loss=True
+  --per_device_train_batch_size=6 \
+  --adam_beta1=0.95 \
+  --adam_beta2=0.99 \
+  --average_tokens_across_devices=True \
+  --copy_paste_prob=0.2 \
+  --dataloader_num_workers=4 \
+  --dataloader_persistent_workers=True \
+  --dataloader_pin_memory=True \
+  --ddp_find_unused_parameters=True \
+  --early_stopping=25 \
+  --ema \
+  --fold=$FOLD \
+  --learning_rate=3e-4 \
+  --max_grad_norm=3 \
+  --mixup_prob=0.2 \
+  --num_train_epochs=75 \
+  --random_erase_prob=0.2 \
+  --use_6_classes=True \
+  --use_cross_entropy_loss=True \
+  --use_instance_crops=True \
+  --use_random_crops=True \
+  --use_stride4=False \
+  --warmup_steps=64 \
+  --x_rotation_limit=10 \
+  --y_rotation_limit=10
 done
--- train_od.py
+++ train_od.py
@@ -1,5 +1,6 @@
 import json
 import os
+import traceback
 import typing
 from datetime import datetime
 from pathlib import Path
@@ -56,215 +57,220 @@ def main():
 
         training_args.output_dir = os.path.join(output_dir_name, f"{timestamp}_{training_args_str}", model_name_slug)
 
-    training_args.master_print(f"Training arguments: {training_args}")
+    try:
+        training_args.master_print(f"Training arguments: {training_args}")
 
-    num_classes = 6 if model_args.use_6_classes else 5
+        num_classes = 6 if model_args.use_6_classes else 5
 
-    extra_model_args = {}
-    if training_args.bf16:
-        extra_model_args["torch_dtype"] = torch.bfloat16
-    training_args.master_print(f"Extra model args: {extra_model_args}")
+        extra_model_args = {}
+        if training_args.bf16:
+            extra_model_args["torch_dtype"] = torch.bfloat16
+        training_args.master_print(f"Extra model args: {extra_model_args}")
 
-    # model_kwargs = build_model_args_from_commandline(model_args)
-    model_kwargs = {}
-    training_args.master_print(f"Model kwargs: {model_kwargs}")
+        # model_kwargs = build_model_args_from_commandline(model_args)
+        model_kwargs = {}
+        training_args.master_print(f"Model kwargs: {model_kwargs}")
 
-    if model_args.model_name == "segresnet_s1":
-        config = SegResNetForObjectDetectionS1Config(
-            num_classes=num_classes,
+        if model_args.model_name == "segresnet_s1":
+            config = SegResNetForObjectDetectionS1Config(
+                num_classes=num_classes,
+            )
+            model = SegResNetForObjectDetectionS1(config)
+        elif model_args.model_name == "segresnetv2":
+            config = SegResNetForObjectDetectionV2Config(
+                use_stride2=model_args.use_stride2,
+                use_stride4=model_args.use_stride4,
+                use_offset_head=model_args.use_offset_head,
+                head_dropout_prob=model_args.head_dropout_prob,
+                num_classes=num_classes,
+            )
+            model = SegResNetForObjectDetectionV2(config)
+        elif model_args.model_name == "dynunet":
+            config = DynUNetForObjectDetectionConfig(
+                use_stride2=model_args.use_stride2,
+                use_stride4=model_args.use_stride4,
+                num_classes=num_classes,
+            )
+            model = DynUNetForObjectDetection(config)
+        elif model_args.model_name == "dynunet_v2":
+            config = DynUNetForObjectDetectionConfig(
+                # dropout=0.1,
+                res_block=True,
+                use_stride2=model_args.use_stride2,
+                use_stride4=model_args.use_stride4,
+                num_classes=num_classes,
+                object_size=32,
+                intermediate_channels=64,
+                offset_intermediate_channels=8,
+            )
+            model = DynUNetForObjectDetection(config)
+        elif model_args.model_name == "hrnet":
+            config = HRNetv2ForObjectDetectionConfig(
+                num_classes=num_classes,
+            )
+            model = HRNetv2ForObjectDetection(config)
+        elif model_args.model_name == "convnext":
+            config = ConvNextForObjectDetectionConfig(
+                num_classes=num_classes,
+            )
+            model = ConvNextForObjectDetection(config)
+        # elif model_args.model_name == "unet3d":
+        #     config = UNet3DForObjectDetectionConfig(window_size=model_args.window_size)
+        #     model = UNet3DForObjectDetection(config)
+        # elif model_args.model_name == "maxvit_nano_unet25d":
+        #     config = MaxVitUnet25dConfig(img_size=model_args.window_size)
+        #     model = MaxVitUnet25d(config)
+        # elif model_args.model_name == "unet3d-fat":
+        #     config = UNet3DForObjectDetectionConfig(
+        #         encoder_channels=[32, 64, 128, 256],
+        #         num_blocks_per_stage=(2, 3, 4, 6),
+        #         num_blocks_per_decoder_stage=(2, 2, 2),
+        #         intermediate_channels=64,
+        #         offset_intermediate_channels=16,
+        #         window_size=model_args.window_size,
+        #     )
+        #     model = UNet3DForObjectDetection(config)
+        # elif model_args.model_name == "unetr":
+        #     config = SwinUNETRForObjectDetectionConfig()
+        #     model = SwinUNETRForObjectDetection(config)
+        else:
+            raise ValueError(f"Unknown model name: {model_args.model_name}")
+
+        if model_args.pretrained_backbone_path is not None:
+            backbone_sd = torch.load(model_args.pretrained_backbone_path, weights_only=True)
+            model.backbone.load_state_dict(backbone_sd, strict=True)
+            training_args.master_print(f"Loaded pretrained backbone from {model_args.pretrained_backbone_path}")
+
+        training_args.master_print(f"Model parameters: {count_parameters(model, human_friendly=True)}")
+
+        # if training_args.gradient_checkpointing:
+        #     model.gradient_checkpointing_enable()
+
+        with fabric.rank_zero_first():
+            data_module = ObjectDetectionDataModule(
+                data_args=data_args,
+                train_args=training_args,
+                model_args=model_args,
+            )
+
+        model_module = ObjectDetectionModel(model=model, data_args=data_args, model_args=model_args, train_args=training_args)
+
+        if training_args.transfer_weights is not None:
+            checkpoint = torch.load(training_args.transfer_weights, map_location="cpu")
+            transfer_weights(model_module, checkpoint["state_dict"])
+            training_args.master_print(f"Loaded weights from {training_args.transfer_weights}")
+
+        checkpoint_callback = ModelCheckpoint(
+            enable_version_counter=False,
+            monitor="val/score",
+            mode="max",
+            save_last=True,
+            auto_insert_metric_name=False,
+            save_top_k=5,
+            filename=f"{timestamp}_{model_name_slug}"
+            + "_{step:03d}-score-{val/score:0.4f}-at-{val/apo-ferritin_threshold:0.3f}-{val/beta-galactosidase_threshold:0.3f}-{val/ribosome_threshold:0.3f}-{val/thyroglobulin_threshold:0.3f}-{val/virus-like-particle_threshold:0.3f}",
+            # + "_{step:03d}-score-{val/score:0.4f}",
         )
-        model = SegResNetForObjectDetectionS1(config)
-    elif model_args.model_name == "segresnetv2":
-        config = SegResNetForObjectDetectionV2Config(
-            use_stride2=model_args.use_stride2,
-            use_stride4=model_args.use_stride4,
-            use_offset_head=model_args.use_offset_head,
-            head_dropout_prob=model_args.head_dropout_prob,
-            num_classes=num_classes,
-        )
-        model = SegResNetForObjectDetectionV2(config)
-    elif model_args.model_name == "dynunet":
-        config = DynUNetForObjectDetectionConfig(
-            use_stride2=model_args.use_stride2,
-            use_stride4=model_args.use_stride4,
-            num_classes=num_classes,
-        )
-        model = DynUNetForObjectDetection(config)
-    elif model_args.model_name == "dynunet_v2":
-        config = DynUNetForObjectDetectionConfig(
-            # dropout=0.1,
-            res_block=True,
-            use_stride2=model_args.use_stride2,
-            use_stride4=model_args.use_stride4,
-            num_classes=num_classes,
-            object_size=32,
-            intermediate_channels=64,
-            offset_intermediate_channels=8,
-        )
-        model = DynUNetForObjectDetection(config)
-    elif model_args.model_name == "hrnet":
-        config = HRNetv2ForObjectDetectionConfig(
-            num_classes=num_classes,
-        )
-        model = HRNetv2ForObjectDetection(config)
-    elif model_args.model_name == "convnext":
-        config = ConvNextForObjectDetectionConfig(
-            num_classes=num_classes,
-        )
-        model = ConvNextForObjectDetection(config)
-    # elif model_args.model_name == "unet3d":
-    #     config = UNet3DForObjectDetectionConfig(window_size=model_args.window_size)
-    #     model = UNet3DForObjectDetection(config)
-    # elif model_args.model_name == "maxvit_nano_unet25d":
-    #     config = MaxVitUnet25dConfig(img_size=model_args.window_size)
-    #     model = MaxVitUnet25d(config)
-    # elif model_args.model_name == "unet3d-fat":
-    #     config = UNet3DForObjectDetectionConfig(
-    #         encoder_channels=[32, 64, 128, 256],
-    #         num_blocks_per_stage=(2, 3, 4, 6),
-    #         num_blocks_per_decoder_stage=(2, 2, 2),
-    #         intermediate_channels=64,
-    #         offset_intermediate_channels=16,
-    #         window_size=model_args.window_size,
-    #     )
-    #     model = UNet3DForObjectDetection(config)
-    # elif model_args.model_name == "unetr":
-    #     config = SwinUNETRForObjectDetectionConfig()
-    #     model = SwinUNETRForObjectDetection(config)
-    else:
-        raise ValueError(f"Unknown model name: {model_args.model_name}")
 
-    if model_args.pretrained_backbone_path is not None:
-        backbone_sd = torch.load(model_args.pretrained_backbone_path, weights_only=True)
-        model.backbone.load_state_dict(backbone_sd, strict=True)
-        training_args.master_print(f"Loaded pretrained backbone from {model_args.pretrained_backbone_path}")
+        loggers = []
 
-    training_args.master_print(f"Model parameters: {count_parameters(model, human_friendly=True)}")
+        report_to = training_args.report_to
+        if isinstance(report_to, list) and len(report_to) == 1:
+            report_to = report_to[0]
 
-    # if training_args.gradient_checkpointing:
-    #     model.gradient_checkpointing_enable()
+        if "tensorboard" in report_to:
+            logger = TensorBoardLogger(save_dir=training_args.output_dir)
+            loggers.append(logger)
+        if "wandb" in report_to:
+            logger = WandbLogger()
+            loggers.append(logger)
+        if len(loggers) == 1:
+            loggers = loggers[0]
 
-    with fabric.rank_zero_first():
-        data_module = ObjectDetectionDataModule(
-            data_args=data_args,
-            train_args=training_args,
-            model_args=model_args,
+        strategy = infer_strategy(training_args, fabric)
+
+        callbacks = [checkpoint_callback]
+
+        if training_args.early_stopping > 0:
+            callbacks.append(EarlyStopping(monitor="val/score", min_delta=0.001, patience=training_args.early_stopping, mode="max"))
+
+        lr_monitor = LearningRateMonitor(logging_interval="step")
+        if "wandb" not in training_args.report_to:
+            callbacks.append(lr_monitor)
+
+        precision = infer_training_precision(training_args)
+        fabric.print(f"Training precision: {precision}")
+
+        if training_args.ema:
+            callbacks.append(EMACallback(decay=BetaDecay(max_decay=training_args.ema_decay, beta=training_args.ema_beta)))
+            fabric.print(f"Using EMA with decay={training_args.ema_decay} and beta={training_args.ema_beta}")
+
+        fabric.print("Batch Size:", training_args.per_device_train_batch_size, training_args.per_device_eval_batch_size)
+
+        trainer = L.Trainer(
+            strategy=strategy,
+            max_epochs=int(training_args.num_train_epochs),
+            max_steps=training_args.max_steps,
+            precision=precision,
+            log_every_n_steps=training_args.logging_steps,
+            default_root_dir=training_args.output_dir,
+            callbacks=callbacks,
+            accumulate_grad_batches=training_args.gradient_accumulation_steps,
+            gradient_clip_val=training_args.max_grad_norm,
+            logger=loggers,
         )
 
-    model_module = ObjectDetectionModel(model=model, data_args=data_args, model_args=model_args, train_args=training_args)
+        trainer.fit(model_module, datamodule=data_module)
 
-    if training_args.transfer_weights is not None:
-        checkpoint = torch.load(training_args.transfer_weights, map_location="cpu")
-        transfer_weights(model_module, checkpoint["state_dict"])
-        training_args.master_print(f"Loaded weights from {training_args.transfer_weights}")
+        # Save hyperparams
+        if trainer.is_global_zero:
+            config = {**training_args.to_dict(), **model_args.to_dict(), **data_args.to_dict()}
+            with open(os.path.join(training_args.output_dir, "config.json"), "w") as f:
+                json.dump(config, f, indent=4, sort_keys=True)
 
-    checkpoint_callback = ModelCheckpoint(
-        enable_version_counter=False,
-        monitor="val/score",
-        mode="max",
-        save_last=True,
-        auto_insert_metric_name=False,
-        save_top_k=5,
-        filename=f"{timestamp}_{model_name_slug}"
-        + "_{step:03d}-score-{val/score:0.4f}-at-{val/apo-ferritin_threshold:0.3f}-{val/beta-galactosidase_threshold:0.3f}-{val/ribosome_threshold:0.3f}-{val/thyroglobulin_threshold:0.3f}-{val/virus-like-particle_threshold:0.3f}",
-        # + "_{step:03d}-score-{val/score:0.4f}",
-    )
+        # Trace & Save
+        models_output_dir = Path(checkpoint_callback.best_model_path).parent
+        best_state_dict = torch.load(checkpoint_callback.best_model_path, map_location=model_module.device, weights_only=True)
+        model_module.load_state_dict(best_state_dict["state_dict"])
 
-    loggers = []
+        window_size = model_args.valid_depth_window_size, model_args.valid_spatial_window_size, model_args.valid_spatial_window_size
 
-    report_to = training_args.report_to
-    if isinstance(report_to, list) and len(report_to) == 1:
-        report_to = report_to[0]
+        if trainer.is_global_zero:
+            trace_model_and_save(window_size, model_module.model, Path(checkpoint_callback.best_model_path).with_suffix(".jit"))
 
-    if "tensorboard" in report_to:
-        logger = TensorBoardLogger(save_dir=training_args.output_dir)
-        loggers.append(logger)
-    if "wandb" in report_to:
-        logger = WandbLogger()
-        loggers.append(logger)
-    if len(loggers) == 1:
-        loggers = loggers[0]
+        best_k_models = list(checkpoint_callback.best_k_models.keys())
+        averaged_filename = f"{timestamp}_{model_name_slug}"
+        tmp_averaged_checkpoint = models_output_dir / f"{averaged_filename}.pt"
 
-    strategy = infer_strategy(training_args, fabric)
+        # Average checkpoint
+        if trainer.is_global_zero:
+            average_checkpoints(*best_k_models, output_path=tmp_averaged_checkpoint)
 
-    callbacks = [checkpoint_callback]
+        if is_dist_avail_and_initialized():
+            torch.distributed.barrier()
 
-    if training_args.early_stopping > 0:
-        callbacks.append(EarlyStopping(monitor="val/score", min_delta=0.001, patience=training_args.early_stopping, mode="max"))
+        model_module.load_state_dict(
+            torch.load(tmp_averaged_checkpoint, map_location=model_module.device, weights_only=True)["state_dict"]
+        )
+        metrics = trainer.validate(model=model_module, datamodule=data_module)
+        print(metrics)
 
-    lr_monitor = LearningRateMonitor(logging_interval="step")
-    if "wandb" not in training_args.report_to:
-        callbacks.append(lr_monitor)
+        # new name
+        metrics_suffix = "averaged-score-{val/score:0.4f}-at-{val/apo-ferritin_threshold:0.3f}-{val/beta-galactosidase_threshold:0.3f}-{val/ribosome_threshold:0.3f}-{val/thyroglobulin_threshold:0.3f}-{val/virus-like-particle_threshold:0.3f}".format(
+            **metrics[0]
+        )
+        new_averaged_filename = f"{timestamp}_{model_name_slug}_{metrics_suffix}"
+        new_averaged_filepath = models_output_dir / f"{new_averaged_filename}.pt"
 
-    precision = infer_training_precision(training_args)
-    fabric.print(f"Training precision: {precision}")
-
-    if training_args.ema:
-        callbacks.append(EMACallback(decay=BetaDecay(max_decay=training_args.ema_decay, beta=training_args.ema_beta)))
-        fabric.print(f"Using EMA with decay={training_args.ema_decay} and beta={training_args.ema_beta}")
-
-    fabric.print("Batch Size:", training_args.per_device_train_batch_size, training_args.per_device_eval_batch_size)
-
-    trainer = L.Trainer(
-        strategy=strategy,
-        max_epochs=int(training_args.num_train_epochs),
-        max_steps=training_args.max_steps,
-        precision=precision,
-        log_every_n_steps=training_args.logging_steps,
-        default_root_dir=training_args.output_dir,
-        callbacks=callbacks,
-        accumulate_grad_batches=training_args.gradient_accumulation_steps,
-        gradient_clip_val=training_args.max_grad_norm,
-        logger=loggers,
-    )
-
-    trainer.fit(model_module, datamodule=data_module)
-
-    # Save hyperparams
-    if trainer.is_global_zero:
-        config = {**training_args.to_dict(), **model_args.to_dict(), **data_args.to_dict()}
-        with open(os.path.join(training_args.output_dir, "config.json"), "w") as f:
-            json.dump(config, f, indent=4, sort_keys=True)
-
-    # Trace & Save
-    models_output_dir = Path(checkpoint_callback.best_model_path).parent
-    best_state_dict = torch.load(checkpoint_callback.best_model_path, map_location=model_module.device, weights_only=True)
-    model_module.load_state_dict(best_state_dict["state_dict"])
-
-    window_size = model_args.valid_depth_window_size, model_args.valid_spatial_window_size, model_args.valid_spatial_window_size
-
-    if trainer.is_global_zero:
-        trace_model_and_save(window_size, model_module.model, Path(checkpoint_callback.best_model_path).with_suffix(".jit"))
-
-    best_k_models = list(checkpoint_callback.best_k_models.keys())
-    averaged_filename = f"{timestamp}_{model_name_slug}"
-    tmp_averaged_checkpoint = models_output_dir / f"{averaged_filename}.pt"
-
-    # Average checkpoint
-    if trainer.is_global_zero:
-        average_checkpoints(*best_k_models, output_path=tmp_averaged_checkpoint)
-
-    if is_dist_avail_and_initialized():
-        torch.distributed.barrier()
-
-    model_module.load_state_dict(
-        torch.load(tmp_averaged_checkpoint, map_location=model_module.device, weights_only=True)["state_dict"]
-    )
-    metrics = trainer.validate(model=model_module, datamodule=data_module)
-    print(metrics)
-
-    # new name
-    metrics_suffix = "averaged-score-{val/score:0.4f}-at-{val/apo-ferritin_threshold:0.3f}-{val/beta-galactosidase_threshold:0.3f}-{val/ribosome_threshold:0.3f}-{val/thyroglobulin_threshold:0.3f}-{val/virus-like-particle_threshold:0.3f}".format(
-        **metrics[0]
-    )
-    new_averaged_filename = f"{timestamp}_{model_name_slug}_{metrics_suffix}"
-    new_averaged_filepath = models_output_dir / f"{new_averaged_filename}.pt"
-
-    if trainer.is_global_zero:
-        torch.save({"state_dict": model_module.state_dict()}, new_averaged_filepath)
-        tmp_averaged_checkpoint.unlink()
-        trace_model_and_save(window_size, model_module.model, new_averaged_filepath.with_suffix(".jit"))
-
+        if trainer.is_global_zero:
+            torch.save({"state_dict": model_module.state_dict()}, new_averaged_filepath)
+            tmp_averaged_checkpoint.unlink()
+            trace_model_and_save(window_size, model_module.model, new_averaged_filepath.with_suffix(".jit"))
+    except Exception as e:
+        with open(os.path.join(training_args.output_dir, f"error_rank_{training_args.local_rank}.log"), "w") as f:
+            f.write(str(e))
+            f.write("\n")
+            f.write(traceback.format_exc())
 
 def build_model_name_slug(data_args, model_args):
     num_classes = 6 if model_args.use_6_classes else 5
