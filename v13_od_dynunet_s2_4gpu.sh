b0e309be822472dd0eadfc9c060bf88d4d421e6a
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2025-01-30 23:18:30 +0200

    V12 V13

--- v13_od_dynunet_s2_4gpu.sh
+++ v13_od_dynunet_s2_4gpu.sh
@@ -1,39 +1,31 @@
 for FOLD in 0 1 2 3 4 5 6
 do
   torchrun --standalone --nproc-per-node=4 train_od.py \
-  --adam_beta1=0.95 --adam_beta2=0.99 \
-  --anisotropic_scale_limit=0.05 \
   --average_tokens_across_devices=True \
   --dataloader_num_workers=8 \
   --dataloader_persistent_workers=True \
   --dataloader_pin_memory=True \
   --ddp_find_unused_parameters=True \
-  --early_stopping=15 \
+  --early_stopping=25 \
   --fold=$FOLD \
-  --learning_rate=5e-5 \
+  --learning_rate=3e-4 \
   --max_grad_norm=3 \
   --ddp_find_unused_parameters=True \
-  --model_name=segresnetv2 \
+  --model_name=dynunet  \
   --num_train_epochs=75 \
-  --per_device_train_batch_size=4 \
-  --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
-  --random_erase_prob=0.2 \
-  --copy_paste_prob=0.25 \
-  --copy_paste_limit=2 \
-  --mixup_prob=0.25 \
-  --scale_limit=0.15 \
+  --per_device_train_batch_size=6 \
+  --scale_limit=0.05 \
   --use_6_classes=True \
   --use_instance_crops=True \
   --use_random_crops=True \
   --use_stride4=False \
-  --version_prefix=v12_ \
+  --version_prefix=v13_ \
   --assigned_min_iou_for_anchor=0.5 \
-  --warmup_steps=64 \
-  --weight_decay=0.01 \
-  --y_rotation_limit=45 \
-  --x_rotation_limit=45 \
+  --warmup_steps=128 \
+  --weight_decay=0.0001 \
+  --x_rotation_limit=5 \
+  --x_rotation_limit=5 \
   --seed=$FOLD \
   --interpolation_mode=1 \
   --validate_on_rot90=True
 done
-
--- v13_od_segresnet_s2_4gpu.sh
+++ v13_od_segresnet_s2_4gpu.sh
@@ -2,7 +2,6 @@ for FOLD in 0 1 2 3 4 5 6
 do
   torchrun --standalone --nproc-per-node=4 train_od.py \
   --adam_beta1=0.95 --adam_beta2=0.99 \
-  --anisotropic_scale_limit=0.05 \
   --average_tokens_across_devices=True \
   --dataloader_num_workers=8 \
   --dataloader_persistent_workers=True \
@@ -17,21 +16,17 @@ do
   --num_train_epochs=75 \
   --per_device_train_batch_size=4 \
   --pretrained_backbone_path=pretrained/wholeBody_ct_segmentation/models/model.pt \
-  --random_erase_prob=0.2 \
-  --copy_paste_prob=0.25 \
-  --copy_paste_limit=2 \
-  --mixup_prob=0.25 \
-  --scale_limit=0.15 \
+  --scale_limit=0.05 \
   --use_6_classes=True \
   --use_instance_crops=True \
   --use_random_crops=True \
   --use_stride4=False \
-  --version_prefix=v12_ \
+  --version_prefix=v13_ \
   --assigned_min_iou_for_anchor=0.5 \
   --warmup_steps=64 \
   --weight_decay=0.01 \
-  --y_rotation_limit=45 \
-  --x_rotation_limit=45 \
+  --y_rotation_limit=5 \
+  --x_rotation_limit=5 \
   --seed=$FOLD \
   --interpolation_mode=1 \
   --validate_on_rot90=True
