4e229972fefa2ca8d847e161d5b08cbda1482884
BloodAxe <ekhvedchenya@gmail.com>
2025-02-03 20:05:03 +0200

    Attempt to set is_pad=False

--- cryoet/modelling/ch_models/adaptors.py
+++ cryoet/modelling/ch_models/adaptors.py
@@ -63,5 +63,7 @@ class MdlCh20dCe2c2_resnet34(FakeObjectDetectionAdapter):
 class MdlCh20dCe2_effnetb3(FakeObjectDetectionAdapter):
     def __init__(self):
         super().__init__()
-        backbone_args = dict(spatial_dims=3, in_channels=1, out_channels=6, backbone="efficientnet-b3", pretrained=False, is_pad=False)
+        backbone_args = dict(
+            spatial_dims=3, in_channels=1, out_channels=6, backbone="efficientnet-b3", pretrained=False, is_pad=False
+        )
         self.backbone = FlexibleUNet(**backbone_args)
